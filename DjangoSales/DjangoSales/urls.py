7070ae7bb8316f8a5255175e8c9e428a148e5bc8
ifonseca <lamgers@gmail.com>
2016-03-07 20:06:49 -0600

    Agregado de configuraciones y urls

--- DjangoSales/DjangoSales/urls.py
+++ DjangoSales/DjangoSales/urls.py
@@ -13,9 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
+from apps.users import urls as users_urls
+from apps.home import urls as home_urls
+from apps.administrador import urls as admin_urls
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^', include(users_urls)),
+    url(r'^venta/', include(home_urls)),
+    url(r'^administrador/', include(admin_urls)),
 ]
