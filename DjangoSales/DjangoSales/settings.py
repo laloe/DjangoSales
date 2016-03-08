e7f6a75fb17dcc8efcf9e5f04cb7cd899ff455ce
laloe <laloe.90@gmail.com>
2016-03-07 19:52:57 -0600

    modificacion settings

--- DjangoSales/DjangoSales/settings.py
+++ DjangoSales/DjangoSales/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'apps.users',
+    'apps.home',
+    'apps.admin',
 ]
 
 MIDDLEWARE_CLASSES = [
